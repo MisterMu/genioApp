a1fe0b1ae2efc08dd65122aa8690840e01e30050
Pakin Suwannawat <painter.suwannawat@gmail.com>
2017-11-01 20:55:08 +0700

    - initialization flow run when app start in app.js

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar, NetInfo } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, NetInfo, AsyncStorage, Modal } from 'react-native';
 import axios from 'axios';
 import { TabNavigator } from 'react-navigation';
-import { DataLoader } from './src/component/';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+
+import { DateInputCard } from './src/component/';
 import { HomeScreen, GameScreen, ScoreScreen, SettingScreen } from './src/screen/';
+import { BgColor } from './src/assest/color.js';
 
 axios.defaults.baseURL = 'http://1101-beta.duckdns.org:8989/genio/api';
 
@@ -34,16 +37,125 @@ NetInfo.isConnected.fetch().then(isConnected => {
 });
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+      modalVisible: false
+    };
+  }
+
+  closeModal = () => {
+    this.setState({modalVisible: false});
+  }
+
+  openModal = () => {
+    this.setState({modalVisible: true});
+  }
+
+  initialData = () => [
+    axios.post('/user/initialData', { user_id: this.state.u_id})
+      .then((res) => {
+        AsyncStorage.multiSet([
+          ['History', JSON.stringify(res.data.model.history)],
+          ['Score', JSON.stringify(res.data.model.score)],
+          ['Games', JSON.stringify(res.data.model.game_list)]
+        ])
+          .then(() => {
+            this.setState({isReady: true})
+          })
+        console.log(res.data.model)
+      })
+      .catch(err => console.error(err))
+  ]
+
+  componentDidMount(){
+    // 1. Have u_id in storage?
+    AsyncStorage.getItem('u_id')
+      .then((value) => {
+        // 1.a No, then get email from Google Signin
+        if (value == null) {
+          GoogleSignin.configure({
+            webClientId: '672546647653-o1jdmpjtls8pk7kdo0o1av1j1ngfsd71.apps.googleusercontent.com',
+            offlineAccess: true,
+            forceConsentPrompt: true,
+            accountName: ''
+          })
+            .then(() => {
+              GoogleSignin.signIn()
+                .then((user) => {
+                  AsyncStorage.setItem('email', user.email);
+                  this.setState({ email: user.email });
+                  // 2. Was this email registered?
+                  axios.post('/user/checkEmail', { email: this.state.email })
+                    .then((res) => {
+                      // 2.a Yes, get u_id from email, get initial data, and go on Genio
+                      if (res.data.success) {
+                        AsyncStorage.setItem('u_id', res.data.user_id + '');
+                        this.setState({u_id: res.data.user_id });
+                        this.initialData();
+                      }
+                      // 2.b No, Launch dateinput, get DoB, registering, get initial data and go on Genio
+                      else {
+                        this.openModal();
+                      }
+                    })
+                    .catch((err) => console.error(err));
+                })
+                .catch((err) => {
+                  switch (err.code) {
+                    case -5:
+                      // It happens on iOS: app launched the sign in screen, but user refuses to sign in (via pressing BACK button on upper left corner)
+                    case 8:
+                      // It happens on android: app launched the signin dialog (for user picking his account), 
+                      // but user dismisses the signin dialog **before** it shows up (either via BACK button or touching outside area of the dialog)
+                    case 12501:
+                      // It happens on android: app launched the signin dialog (for user picking his account),
+                      // but user dismisses the signin dialog **after** it shows up (either via BACK button or touching outside area of the dialog)
+        
+                      // all above error codes reflect that user refused to sign in. The app should provide consequence according to your app's nature.
+                      break;
+                    default:
+                      // other error codes means sign-in really failed, we possibly need to display some error message, or even ask user to try sign in again.
+                      break;
+                  }
+                })
+            })
+            .catch(err => console.error(err));
+        }
+        // 1.b Yes, get initial data and go on Genio
+        else {
+          console.log('USER_ID', value);
+          this.setState({ u_id: value });
+          this.initialData();
+        }
+      })
+      .catch(err => console.error(err))
+}
+
   render() {
-    return (
-      <View style={{flex: 1}}>
-        <DataLoader />
-        <View style={styles.statusBar}/>
-        <View style={styles.container}>
-          <AppNavigator/>
+    if (this.state.isReady === false) {
+      return (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {return null}}
+        >
+          <View style={styles.modal}>
+            <DateInputCard email={this.state.email} done={() => {this.closeModal(); this.initialData();}}/>
+          </View>
+        </Modal>
+      );
+    } else {
+      return (
+        <View style={{flex: 1}}>
+          <View style={styles.container}>
+            <AppNavigator/>
+          </View>
         </View>
-      </View>
-    );
+      );
+    }
   }
 }
 
@@ -52,9 +164,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center'
   },
-  statusBar: {
-    backgroundColor: 'white',
-    alignSelf: 'stretch',
-    height: StatusBar.currentHeight
+  modal: {
+    flex: 1,
+    backgroundColor: BgColor,
+    justifyContent: 'center'
   }
 });
