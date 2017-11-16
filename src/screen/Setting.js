a5b0ee4f298f4633e316b6f591f8b3e2407249e6
Pakin Suwannawat <painter.suwannawat@gmail.com>
2017-11-16 18:14:05 +0700

    add setting card component to setting screen

--- src/screen/Setting.js
+++ src/screen/Setting.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Image, Alert, ScrollView } from 'react-native';
-import { AppBar, Button, DateInputCard } from '../component'
+import { StyleSheet, View, Image, Alert, ScrollView, AsyncStorage, ActivityIndicator } from 'react-native';
+import { AppBar, Button, DateInputCard, SettingCard } from '../component'
 import { BgColor } from '../../assets/color.js';
 
 export class SettingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      DoB: null,
+      gender: null,
+      privacy: null,
+      isReady: false
+    }
+  }
   static navigationOptions = {
     tabBarLabel: 'ตั้งค่า',
     tabBarIcon: ({ tintColor }) => (
@@ -14,15 +23,35 @@ export class SettingScreen extends React.Component {
     ),
   };
 
+  componentDidMount() {
+    AsyncStorage.getItem('Profile').then((val) => {
+      this.setState({
+        DoB: JSON.parse(val).date_of_birth,
+        gender: JSON.parse(val).gender,
+        privacy: JSON.parse(val).privacy_status,
+        isReady: true
+      });
+    });
+  }
+
   render() {
-    return (
-      <View style={styles.host}>
-        <AppBar title="ตั้งค่า"/>
-        <ScrollView contentContainerStyle={styles.content}>
-          {/* <DateInputCard/> */}
-        </ScrollView>
-      </View>
-    );
+    if (this.state.isReady) {
+      console.log(this.state)
+      return (
+        <View style={styles.host}>
+          <AppBar title="ตั้งค่า"/>
+          <ScrollView contentContainerStyle={styles.content}>
+            <SettingCard data={this.state}/>
+          </ScrollView>
+        </View>
+      );
+    } else {
+      return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator/>
+        </View>
+      );
+    }
   }
 }
 
@@ -32,8 +61,8 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    paddingHorizontal: 40,
     paddingVertical: 30,
+    paddingHorizontal: 40,
     backgroundColor: BgColor,
   }
 });
